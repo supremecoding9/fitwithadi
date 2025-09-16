b5299867b5ac4438e2abab1c6c0552ddc689a453
Supreme Coding <support@mytruckmate.com>
2025-09-16 19:56:16 +0000

    Add fitwithadi theme with correct .gitignore

---
+++ wp-content/themes/fitwithadi/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Main template file.
+ *
+ * @package FitWithAdi
+ */
+
+get_header();
+?>
+<main id="main-content" class="site-main">
+    <?php get_template_part( 'template-parts/content', 'home' ); ?>
+</main>
+<?php
+get_footer();
